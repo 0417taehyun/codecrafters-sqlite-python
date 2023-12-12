46b44f72530213419b0c69e3071cb9f849e8c196
taehyun <0417taehyun@gmail.com>
2023-12-12 14:23:39 +0900

    feat: Print page size

--- app/main.py
+++ app/main.py
@@ -9,12 +9,18 @@ command = sys.argv[2]
 
 if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print("Logs from your program will appear here!")
 
-        # Uncomment this to pass the first stage
-        # database_file.seek(16)  # Skip the first 16 bytes of the header
-        # page_size = int.from_bytes(database_file.read(2), byteorder="big")
-        # print(f"database page size: {page_size}")
+        # Database header
+        # The first 100 bytes of the database file comprise the database file header.
+        
+        # From 0 to 16 bytes of the database file is the header string: "SQLite format 3\000."
+        database_file.seek(16)
+
+        # From 16 to 18 bytes of the database file is the database page size in bytes.
+        # All multibyte fields in the database file header are stored with the most significant byte first (big-endian).
+        # This is why we use "big" for byteorder.
+        page_size: int = int.from_bytes(database_file.read(2), byteorder="big")
+        print(f"database page size: {page_size}")
 else:
     print(f"Invalid command: {command}")
+    
